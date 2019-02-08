ff1aeecb6c45d1c5e21bd89693a07ea04627662e
Hush Now, Quiet Now <hushnowquietnow@gmail.com>
2019-02-07 17:16:08 -0800

    Add nod to Rails 5 credentials in initializer template

--- lib/generators/templates/knock.rb
+++ lib/generators/templates/knock.rb
@@ -1,5 +1,4 @@
 Knock.setup do |config|
-
   ## Expiration claim
   ## ----------------
   ##
@@ -8,7 +7,6 @@ Knock.setup do |config|
   ##
   ## Default:
   # config.token_lifetime = 1.day
-
 
   ## Audience claim
   ## --------------
@@ -37,6 +35,10 @@ Knock.setup do |config|
   ##
   ## Default:
   # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  ##
+  ## If you are using Rails >= 5.2, and are using credentials instead of
+  ## secrets, use this instead:
+  # config.token_secret_signature_key = -> { Rails.application.credentials.secret_key_base }
 
   ## If using Auth0, uncomment the line below
   # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
